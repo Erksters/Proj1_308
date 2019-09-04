bc9d6c16991dc5354c81ec5dcdd262024e601f48
Erksters <51130557+Erksters@users.noreply.github.com>
2019-09-04 13:42:13 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+/**********************************************
+ * Name: ERICK SAENZ-GARDEA                   *
+ * Date: SEPT 8TH, 2019                       *
+ * Assignment: Project 1 -Sequence and Order validation*
+ * **********************************************
+ *  This program will be able to read text files line-by-line and
+ *  validate the bracket expression. If the brackets are miswritten, the program will
+ *  specify which line the error is found*
+ *  *********************************************/
+
+#include <iostream>
+
+int main() {
+    FILE *file = fopen("C:\\Users\\erkst\\CLionProjects\\operational\\test.txt","r");
+    char line[256];
+printf("%s","just .c")
+
+    while (fgets(line, sizeof(li/**********************************************
+ * Name: ERICK SAENZ-GARDEA                   *
+ * Date: SEPT 8TH, 2019                       *
+ * Assignment: Project 1 -Sequence and Order validation*
+ * **********************************************
+ *  This program will be able to read text files line-by-line and
+ *  validate the bracket expression. If the brackets are miswritten, the program will
+ *  specify which line the error is found*
+ *  *********************************************/
+
+#include <iostream>
+
+        int main() {
+            FILE *file;
+            file = fopen("test.txt", "r");
+
+
+            for(int i = 0; fgets(output, sizeof(output),file) != NULL; i++){
+                printf("%s", i, output);
+            }
+            fclose(file);
+            return 0;
+        }
+    ne), file)) {
+        printf("%s", line);
+    }
+    fclose(file);
+    return 0;
+
+
+}
